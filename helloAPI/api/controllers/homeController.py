265c2c900b564fd9811a2257c7c59a410db43f00
thales raymond <thalesfraymond@gmail.com>
2018-09-10 12:22:07 -0300

    using pipenv and starting sql stuff

--- helloAPI/api/controllers/homeController.py
+++ helloAPI/api/controllers/homeController.py
@@ -1,8 +1,9 @@
 from api import app
 from flask import Blueprint
+from flask import jsonify
 
 home_blueprint = Blueprint('home_blueprint', __name__, template_folder='templates', url_prefix='/home')
 
 @home_blueprint.route('/')
 def get():
-    return ":D"
+    return jsonify([1,2,3,4,5,6,7,8,9,10])
