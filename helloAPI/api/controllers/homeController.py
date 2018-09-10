1fcc6291de36c944756fea8593afcc13c9620ea3
thales raymond <thalesfraymond@gmail.com>
2018-09-10 13:49:44 -0300

    First API going to database and returting to API

--- helloAPI/api/controllers/homeController.py
+++ helloAPI/api/controllers/homeController.py
@@ -1,9 +1,13 @@
-from api import app
 from flask import Blueprint
 from flask import jsonify
 
+from infrastructure.repository.appVersionRepository import AppVersionRepository
+
 home_blueprint = Blueprint('home_blueprint', __name__, template_folder='templates', url_prefix='/home')
+
 
 @home_blueprint.route('/')
 def get():
-    return jsonify([1,2,3,4,5,6,7,8,9,10])
+    repository = AppVersionRepository()
+    app_versions = repository.get_app_versions()
+    return jsonify(app_versions)
---
+++ helloAPI/infrastructure/repository/engineFactory.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from config import parser
+
+databaseHost = parser.get('DATABASE', 'HOST')
+databaseUser = parser.get('DATABASE', 'USER')
+databasePassword = parser.get('DATABASE', 'PASSWORD')
+databaseName = parser.get('DATABASE', 'DATABASE_NAME')
+
+connection_string = f'mssql+pyodbc://{databaseUser}:{databasePassword}@{databaseHost}/{databaseName}?driver=SQL+Server'
+
+engine = create_engine(connection_string)
+
+print(connection_string)
