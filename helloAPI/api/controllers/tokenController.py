71a842eda3cbe03b0d0ee8f5e1fb3abc8b000bc8
thales raymond <thalesfraymond@gmail.com>
2018-09-11 14:27:56 -0300

    removed auth package (need more study before implementation)

--- helloAPI/api/controllers/tokenController.py
+++ helloAPI/api/controllers/tokenController.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-from flask_httpauth import HTTPBasicAuth
 
 
 token_blueprint = Blueprint('token_blueprint', __name__, template_folder='views', url_prefix='/token')
--- helloAPI/api/start.py
+++ helloAPI/api/start.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_httpauth import HTTPTokenAuth
 
 from api.controllers import appVersionController, tokenController
 
 app = Flask(__name__)
-auth = HTTPTokenAuth(scheme='Token')
 
 if __name__ == '__main__':
     app.run()
